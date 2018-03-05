ecd221058ba1717b7dd89c6ecb299a92c3f9929b
Emmely <e.a.c.l@hotmail.com>
2018-03-05 15:36:07 +0100

    DataDrivenTestGetTotalExcessServings

---
+++ 1SemesterEksamen/test/first_semester_eksamen/DataDrivenTestGetTotalExcessServings.java
@@ -0,0 +1,60 @@
+package first_semester_eksamen;
+
+        import impl.ServingHandlerImpl;
+import impl.ServingImpl;
+import impl.Time;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*Comment*/
+@RunWith(Parameterized.class)
+public class DataDrivenTestGetTotalExcessServings {
+
+
+    private int amount1;
+    private int amount2;
+    private int amount3;
+    private int expResult;
+
+    @Parameterized.Parameters
+    public static Collection spreadsheetData() throws IOException {
+        InputStream spreadsheet = new FileInputStream("gettotalexcessservingss.xls");
+        return new SpreadsheetData(spreadsheet).getData();
+    }
+
+    public DataDrivenTestGetTotalExcessServings(double amount1, double amount2, double amount3,  double expResult) {
+        super();
+        this.amount1 = (int) amount1;
+        this.amount2 = (int) amount2;
+        this.amount3 = (int) amount3;
+        this.expResult = (int) expResult;
+    }
+
+    @Test
+    public void getExcess() {
+        ServingHandler instance = new ServingHandlerImpl();
+        ArrayList<Serving> servings = new ArrayList<Serving>();
+        Time t = new Time("10:01");
+        servings.add(new ServingImpl("24-12-2017", t, this.amount1, "André"));
+        servings.add(new ServingImpl("24-12-2017", t, this.amount2, "André"));
+        servings.add(new ServingImpl("24-12-2017", t, this.amount3, "Sofia"));
+
+        int result = instance.getTotalExcessServings(servings);
+        assertEquals(this.expResult, result);
+    }
+
+
+
+
+
+
+}
